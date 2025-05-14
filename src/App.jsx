6766cb782acf5eb5759f0c8a876d53e06b64661e
AliEltouny <Eltounyali2006@gmail.com>
2025-05-14 22:02:36 +0100

    Added NavBar & FeatureCards & LogoSection

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
+import NavBar from "./components/NavBar";
+import LogoSection from "./sections/LogoSection";
 import Hero from "./sections/Hero";
 import ShowcaseSection from "./sections/ShowcaseSection";
+import FeatureCards from "./sections/FeatureCards";
 
 const App = () => {
   return (
     <>
+      <NavBar />
       <Hero />
       <ShowcaseSection />
+      <LogoSection />
+      <FeatureCards />
     </>
   )
 }
