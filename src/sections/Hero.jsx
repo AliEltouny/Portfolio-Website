5d0d79b03e01d8d6ef2ad0faba2d9d0b8418d27b
Ali Eltouny <Eltounyali2006@gmail.com>
2025-09-03 10:47:07 +0300

    Updated Hero

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -54,7 +54,7 @@ const Hero = () => {
                             <h1>that Deliver Results</h1>
                         </div>
                     <p1 className="text-white-50 md:text-xl relitive z-10 pointer-events-none">
-                        Hi, I'm Aly, a develioper and designer based in Egypt with a passion for code
+                        Hi, I'm Ali, a developer and designer based in Egypt with a passion for code
                     </p1>
                     <Button 
                         className="md:w-80 md:h-16 w-60 h-12"
